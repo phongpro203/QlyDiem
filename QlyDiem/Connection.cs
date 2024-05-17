b7aa52b11a802fde8cb5f05ea658ec0343a5e280
phongpro203 <phongsmallant@gmail.com>
2024-05-17 09:06:01 +0700

    conenction

--- QlyDiem/Connection.cs
+++ QlyDiem/Connection.cs
@@ -9,7 +9,7 @@ namespace QlyDiem
 {
     internal class Connection
     {
-        private static string sqlcoconnectstring = "server=DESKTOP-E2UORV7\\TESTDB;database=quanlydiem;integrated security=true";
+        private static string sqlcoconnectstring = "server=(local);database=Qlydiem;integrated security=true";
         public static SqlConnection getSqlConnection()
         {
             return new SqlConnection(sqlcoconnectstring);
